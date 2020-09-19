dbdb117a0dcdab3a70fcb62167e365677f753f69
Sophia Castillo <Sphcastillo@gmail.com>
2020-09-19 13:14:26 -0700

    try to fix js attached to devoured buttons

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,7 +9,7 @@ $(function() {
       };
   
       // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newSleepState
       }).then(
@@ -26,8 +26,7 @@ $(function() {
       event.preventDefault();
   
       var newBurger = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+        name: $("#burger").val().trim()
       };
   
       // Send the POST request.
@@ -43,19 +42,6 @@ $(function() {
       );
     });
   
-    $(".delete-cat").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted cat", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+
   });
   
